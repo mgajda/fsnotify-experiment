b5727df60ddea3c72e0c7c4ec7cb44d9bef5078d
Michal J. Gajda <mjgajda@migamake.com>
2020-10-21 22:19:16 +0200

    Watch directories given in the command line.

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,19 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
-import Control.Monad(forever)
+import Control.Monad(forever, forM_)
 import Control.Concurrent
 import Control.Concurrent.Chan as Chan
 import System.FSNotify
+import System.Environment
 
 main = do
   evtChan <- Chan.newChan
+  args <- getArgs
+  putStrLn $ "CLI arguments: " <> show args
   forkIO $ forever $ readChan evtChan >>= print
   withManagerConf (defaultConfig { confDebounce = NoDebounce
-                                 , confUsePolling = False }) $ \mgr -> do
-    System.FSNotify.watchTreeChan mgr "." (const True) evtChan
+                                 , confUsePolling = True }) $ \mgr -> do
+    forM_ args $ \dirname -> 
+      System.FSNotify.watchTreeChan mgr dirname (const True) evtChan
     threadDelay 10000000000
